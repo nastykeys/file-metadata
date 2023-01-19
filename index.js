c7de2ea22fa0556703c4f301a2f89e945b3ab83d
Erinne Denzel <45709222+nastykeys@users.noreply.github.com>
2023-01-19 16:37:04 +0100

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
-
+const upload = multer({ dest: 'public/uploads/'})
 var app = express();
+const fs = require('fs')
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,20 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+  fs.unlink(req.file.path, err => {
+    if(err){
+      console.log(err)
+    }
+  })
+})
